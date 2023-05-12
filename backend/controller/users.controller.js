09646c1761b6759798a20ebcfa77b78ecba434e2
Theeraporn <63072072+heroclik@users.noreply.github.com>
2023-05-12 18:49:20 +0700

    user
    //Get user by ID
    // Update user

--- backend/controller/users.controller.js
+++ backend/controller/users.controller.js
@@ -1,1 +1,43 @@
-var jwt = require('jsonwebtoken');
+const User = require('../models/users.model.js');
+
+// Get user by ID
+async function getUserById(req, res) {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error('Error getting user by ID:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+// Update user
+async function updateUser(req, res) {
+  try {
+    const { userId } = req.params;
+    const { name, email } = req.body;
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+
+    await user.save();
+
+    return res.status(200).json({ message: 'User updated successfully', user });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+module.exports = { getUserById, updateUser };
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,6 +1,12 @@
-const router = require("express").Router();
-const pool = require("../configs/database.js");
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/users.controller');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
+// Get user by ID
+router.get('/:userId', authenticateToken, userController.getUserById);
 
+// Update user
+router.put('/:userId', authenticateToken, userController.updateUser);
 
 module.exports = router;
